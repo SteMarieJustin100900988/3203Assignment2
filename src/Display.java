cf7983f2a08679895f318ef7253bc83251383154
SteMarieJustin100900988 <justin.stemarie@carleton.ca>
2016-12-11 19:52:23 -0500

    Circles now draw

--- src/Display.java
+++ src/Display.java
@@ -6,31 +6,58 @@ public class Display {
 	
 	AlgorithmSetup data;
 	JFrame window;
+	JPanel panel;
 
 	Display (AlgorithmSetup algoData){
 		data = algoData;
 
 		window = new JFrame();
-		window.add(new JPanel(new BorderLayout()));
+		window.setSize(720, 600);
+		
 		
 		if(data.barGraph){
-			//setup for displaying movement
+			//Draw the circles on the line
+			panel = new JPanel(new BorderLayout()) {
+				public void paintComponent(Graphics g){
+					g.drawLine(10, window.getHeight()/2, window.getWidth()-25, window.getHeight()/2);
+					for (int i=0; i<data.nodes.size(); i++){
+					//draw circle(s) at location
+						int diameter = (int) ((window.getWidth()-25-10)*(2 * data.r));
+						int xPos = (int) (10+(window.getWidth()-25-10)*data.nodes.get(i)-(diameter/2));
+						
+						g.drawOval(xPos, window.getHeight()/2-(diameter/2), diameter, diameter);
+
+						g.drawLine(xPos+(diameter/2), window.getHeight()/2-(diameter/2), xPos+(diameter/2), window.getHeight()/2+(diameter/2));
+					}
+				}
+			};
+			
 		} else {
-			//setup for displaying line graph
+			//draw the line graph
+			panel = new JPanel(new BorderLayout()) {
+				public void paintComponent(Graphics g){
+					//do the thing
+				}
+			};
 		}
-		
+
+		window.add(panel);
+		window.setVisible(true);
 		
 	}
 
 	
 	/*
-	 * Draws the current state as a line
+	 * Updates the current state, then draws it as a line
 	 */
-	public void drawCurrentState(){
-		//draw line
-		for (int i=0; i<data.nodes.size(); i++){
-			//draw circle(s) at location
+	public void updateCurrentState(){
+		//can be edited to change the algorithm, currently only does 1
+		switch(data.algoNum){
+			default:
+				break;
 		}
+		
+		panel.repaint();
 	}
 	
 	
